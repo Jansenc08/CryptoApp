a00b83a0308a5681b488f5edfd6888664b924c02
Jansenc08 <jansencastillo08@gmail.com>
2025-07-02 17:44:15 +0800

    feat: Implemented pagination by detecting scroll bottom

---
+++ CryptoApp/Shared/LoadingView/LoadingView.h
@@ -0,0 +1,12 @@
+//
+//  LoadingView.h
+//  CryptoApp
+//
+//  Created by Jansen Castillo on 2/7/25.
+//
+
+#ifndef LoadingView_h
+#define LoadingView_h
+
+
+#endif /* LoadingView_h */
---
+++ CryptoApp/Shared/LoadingView/LoadingView.m
@@ -0,0 +1,8 @@
+//
+//  LoadingView.m
+//  CryptoApp
+//
+//  Created by Jansen Castillo on 2/7/25.
+//
+
+#import <Foundation/Foundation.h>
