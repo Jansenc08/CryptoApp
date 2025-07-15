9c34af5f3335d8b00c2bb1b784d1d61913f1295b
Jansenc08 <jansencastillo08@gmail.com>
2025-07-15 14:22:29 +0800

    chore: Deleted unused files

--- CryptoApp/Helpers/ThemeManagerColors.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ThemeManagerColors.swift
-//  CryptoApp
-//
-//  Created by Jansen Castillo on 15/7/25.
-//
-
-import Foundation
--- CryptoApp/Helpers/ThemeManagerFonts.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ThemeManagerFonts.swift
-//  CryptoApp
-//
-//  Created by Jansen Castillo on 15/7/25.
-//
-
-import Foundation
